9dfa9e0ddc98be6a0a4573fc4d1e95371cb7d8b1
tinvariable <tinvariable578@gmail.com>
2015-01-20 17:56:21 -0700

    PID
    Figuring out PID

--- RobotCode/src/org/usfirst/frc/team1891/robot/AutonomousMaster.java
+++ RobotCode/src/org/usfirst/frc/team1891/robot/AutonomousMaster.java
@@ -1,5 +1,15 @@
 package org.usfirst.frc.team1891.robot;
 
-public class AutonomousMaster {
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+
+public class AutonomousMaster 
+{
+	double P;
+	double I;
+	double D;
+	double F;
+	PIDOutput pidOutput;
+	PIDSource pidInput;
 
 }
